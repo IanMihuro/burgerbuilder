79492eda7cd454dabebd484d64de0fb159cd8f95
Ian.Mihuro <nickmihuro@gmail.com>
2018-10-23 21:00:00 +0300

    updated build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,22 @@ class BurgerBuilder extends Component{
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+        .map(igKey =>{
+            //console.log("ingredients[igKey]", ingredients[igKey]);
+            return ingredients[igKey];
+        })
+        .reduce((sum, el) =>{
+            //console.log("sum + el", sum + el)
+            return sum + el;
+        }, 0);
+        this.setState({purchasable: sum>0});
+
     }
 
     addIngredientHandler = (type) => {
@@ -38,16 +53,48 @@ class BurgerBuilder extends Component{
             totalPrice: newPrice,
             ingredients: updatedIngredients
         });
+        this.updatePurchaseState(updatedIngredients);
     }
 
-    removeIngredientHandler = (type) => {} 
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        });
+        this.updatePurchaseState(updatedIngredients);
+    } 
 
     render() {
+        const disabedInfo = {
+            ...this.state.ingredients
+        };
+        for(let key in disabedInfo) {
+            disabedInfo[key] = disabedInfo[key] <= 0
+        }
+
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
-                ingredientAdded={this.addIngredientHandler}/>
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabedInfo}
+                    price={this.state.totalPrice}
+                    purchasable={this.state.purchasable}
+                />
             </Aux>
         );
     }
