d35f1dee6c075ad76f60b0b6e0827dbf595b90b9
Ian.Mihuro <nickmihuro@gmail.com>
2018-11-04 21:34:15 +0300

    Added price to the modal and custom button components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,6 +88,10 @@ class BurgerBuilder extends Component{
         this.setState({ purchasing: false });
     }
 
+    purchaseContinueHandler =() => {
+        alert('You continue!');
+    }
+
     render() {
         const disabedInfo = {
             ...this.state.ingredients
@@ -99,7 +103,12 @@ class BurgerBuilder extends Component{
         return(
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinue={this.purchaseContinueHandler}
+                        price={this.state.totalPrice}
+                        />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
